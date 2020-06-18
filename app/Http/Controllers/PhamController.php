27fbbd51e2b3d01675390333e2e827920dfb5e8d
maithanhduyan <tiachop0102@gmail.com>
2020-06-18 14:58:19 +0700

    fix pham was redirected not font show

--- app/Http/Controllers/PhamController.php
+++ app/Http/Controllers/PhamController.php
@@ -19,6 +19,7 @@ class PhamController extends Controller
     public function show($pham = NULL)
     {
         try {
+
             $pham_list = Pham::all();
             $baike_list = BaiKe::where('pham', $pham)
                 ->orderBy('number', 'asc')
@@ -26,23 +27,12 @@ class PhamController extends Controller
 
             $pham_name = Pham::where('name', $pham)->first();
 
-            if (count($baike_list) == 0) {
-                return redirect()->action(
-                    'BaiKeController@show',
-                    ['baike' => 117]
-                );
-            }
-
             return view('pham', [
                 'pham' => $pham_name,
                 'pham_list' => $pham_list,
                 'baike_list' => $baike_list,
             ]);
         } catch (Exception $ex) {
-            return redirect()->action(
-                'BaiKeController@show',
-                ['number' => 117]
-            );
         }
     }
 }
